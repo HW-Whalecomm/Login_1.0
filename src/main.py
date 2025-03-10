b8d7aad7621c27dec42e72a64cfb23e01000288e
MarckDev <carbajalrmarco@gmail.com>
2025-03-10 13:07:42 -0600

    Se agregaron las vistas de login y signup así como sus botones para cambiar entre las mismas

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 import flet as ft
+from router import views_handler
 
 def main(page:ft.Page):
     def route_change(route):
-        pass
+        page.views.clear()
+        page.views.append(views_handler(page)[page.route])
+        page.update()
 
     page.on_route_change = route_change
+  
+    page.go("/login")
+
 
 
 ft.app(target=main, assets_dir = "assets")
