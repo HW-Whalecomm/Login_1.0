3e10dd32c30b2f46bfce7b8d2605b5f752113257
MarckDev <carbajalrmarco@gmail.com>
2025-06-04 11:52:10 -0600

    Cambio de las diagonales en mqtt_data requests

--- src/pages/dashboard/components/utils/mqtt_data.py
+++ src/pages/dashboard/components/utils/mqtt_data.py
@@ -9,9 +9,9 @@ dummy_data={}
 def request_data():
    global dummy_data
 
-   medidores = requests.get("https://nkldhxv7pi.execute-api.us-east-1.amazonaws.com/consulta")
-   lectura = requests.get("https://nkldhxv7pi.execute-api.us-east-1.amazonaws.com/lectura")
-   status = requests.get("https://nkldhxv7pi.execute-api.us-east-1.amazonaws.com/status")
+   medidores = requests.get("https:\\nkldhxv7pi.execute-api.us-east-1.amazonaws.com\consulta")
+   lectura = requests.get("https:\\nkldhxv7pi.execute-api.us-east-1.amazonaws.com\lectura")
+   status = requests.get("https:\\nkldhxv7pi.execute-api.us-east-1.amazonaws.com\status")
 
    medidores = json.loads(medidores.text)
    lectura = json.loads(lectura.text)
